58b179a66214fcb0a0c3d5dd9860505abc6b5375
enkhuush <b.erdenemyagmar@gmail.com>
2023-11-23 12:41:41 +0800

    undsen heseg urgeljll

--- lib/home.dart
+++ lib/home.dart
@@ -68,10 +68,131 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ),
                       ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 140, left: 20),
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Нийт үлдэгдэл",
+                                  style: TextStyle(
+                                      color: Color(0xFFFFFFFFF),
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Icon(
+                                  Icons.keyboard_arrow_up,
+                                  color: Color(0xFFFFFFFF),
+                                  size: 20,
+                                ),
+                                SizedBox(
+                                  width: 150,
+                                ),
+                                Icon(
+                                  Icons.more_horiz,
+                                  color: Color(0xFFFFFFFFF),
+                                  size: 25,
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "\$ 5,232.00",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 25,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.arrow_downward_rounded,
+                                  color: Color(0xFFFFFFFF),
+                                  size: 20,
+                                ),
+                                Text(
+                                  "Орлого",
+                                  style: TextStyle(
+                                    color: Color(0xCCFFFFFF),
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 150,
+                                ),
+                                Icon(
+                                  Icons.arrow_upward_rounded,
+                                  color: Color(0xFFFFFFFF),
+                                  size: 20,
+                                ),
+                                Text(
+                                  "Зарлага",
+                                  style: TextStyle(
+                                    color: Color(0xCCFFFFFF),
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "\$ 1,840.00",
+                                  style: TextStyle(
+                                      color: Color(0xFFFFFFFFF),
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  width: 120,
+                                ),
+                                Text(
+                                  "\$ 284.00",
+                                  style: TextStyle(
+                                      color: Color(0xFFFFFFFFF),
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
-                )
+                ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Гүйлгээний түүх",
+                    style: TextStyle(
+                        color: Color(0xFF222222),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18),
+                  ),
+                  Text(
+                    "Бүгдийг харах",
+                    style: TextStyle(
+                      color: Color(0xFF666666),
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
